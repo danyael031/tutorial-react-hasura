55a42ec7b29ccae447ab0877430d11dfcddda9e7
Dan Yael Sajaropulos Verdugo <danyael031@gmail.com>
2021-02-04 00:09:46 -0700

    Llegando hasta la sección 3.3-clear-completed del tutorial

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+import {GET_MY_TODOS} from './TodoPrivateList';
+
+// Las queries de GQL soportan variables las cuales son pasadas como
+// parámetros en el hook.
+const ADD_TODO = gql`
+  mutation ($todo: String!, $isPublic: Boolean!) {
+    insert_todos(objects: {title: $todo, is_public: $isPublic}) {
+      affected_rows
+      returning {
+        id
+        title
+        created_at
+        is_completed
+      }
+    }
+  }
+ `;
 
 const TodoInput = ({ isPublic = false }) => {
+  const [todoInput, setTodoInput] = useState('');
+
+  const updateCache = (cache, {data}) => {
+    // If this is for the public feed, do nothing
+    if (isPublic) {
+      return null;
+    }
+    // Fetch the todos from the cache
+    const existingTodos = cache.readQuery({
+      query: GET_MY_TODOS
+    });
+    // Add the new todo to the cache
+    const newTodo = data.insert_todos.returning[0];
+    cache.writeQuery({
+      query: GET_MY_TODOS,
+      data: {todos: [newTodo, ...existingTodos.todos]}
+    });
+  };
+
+    const resetInput = () => {
+    setTodoInput('');
+  };
+
+  const [addTodo] = useMutation(ADD_TODO, {
+	  // Al hook useMutation se le pueden añadir callbacks para
+	  // determinados eventos (ej. update)
+    update: updateCache,
+    onCompleted: resetInput
+  });
   return (
     <form
       className="formInput"
       onSubmit={e => {
         e.preventDefault();
+        addTodo({ variables: { todo: todoInput, isPublic } });
       }}
     >
-      <input className="input" placeholder="What needs to be done?" />
+      <input
+        className="input"
+        placeholder="What needs to be done?"
+        value={todoInput}
+        onChange={e => (setTodoInput(e.target.value))}
+      />
       <i className="inputMarker fa fa-angle-right" />
     </form>
   );
--- src/components/Todo/TodoPrivateList.js
+++ src/components/Todo/TodoPrivateList.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from "react";
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
@@ -14,49 +14,70 @@ const GET_MY_TODOS = gql`
   }
  }`;
 
+ const CLEAR_COMPLETED = gql`
+    mutation clearCompleted {
+      delete_todos(where: {is_completed: {_eq: true}, is_public: {_eq: false}}) {
+        affected_rows
+      }
+    }
+  `;
+
 const TodoPrivateList = props => {
-  const [state, setState] = useState({
-    filter: "all",
-    clearInProgress: false,
+	const [state, setState] = useState({
+		filter: "all",
+		clearInProgress: false,
 
-  });
+	});
 
-  const filterResults = filter => {
-    setState({
-      ...state,
-      filter: filter
-    });
-  };
+	const filterResults = filter => {
+		setState({
+			...state,
+			filter: filter
+		});
+	};
 
-  const clearCompleted = () => { };
-  const { todos } = props;
-  let filteredTodos = todos;
-  if (state.filter === "active") {
-    filteredTodos = todos.filter(todo => todo.is_completed !== true);
-  } else if (state.filter === "completed") {
-    filteredTodos = todos.filter(todo => todo.is_completed === true);
-  }
+	const [clearCompletedTodos] = useMutation(CLEAR_COMPLETED);
 
-  const todoList = [];
-  filteredTodos.forEach((todo, index) => {
-    todoList.push(<TodoItem key={index} index={index} todo={todo} />);
-  });
 
-  return (
-    <Fragment>
-      <div className="todoListWrapper">
-        <ul>{todoList}</ul>
-      </div>
+	const clearCompleted = () => { 
+		clearCompletedTodos({
+			optimisticResponse: true,
+			update: (cache, {data}) => {
+				const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+				const newTodos = existingTodos.todos.filter(t => (!t.is_completed));
+				cache.writeQuery({query:GET_MY_TODOS, data: {todos: newTodos}});
+			}
+		});
 
-      <TodoFilters
-        todos={filteredTodos}
-        currentFilter={state.filter}
-        filterResultsFn={filterResults}
-        clearCompletedFn={clearCompleted}
-        clearInProgress={state.clearInProgress}
-      />
-    </Fragment>
-  );
+	};
+	const { todos } = props;
+	let filteredTodos = todos;
+	if (state.filter === "active") {
+		filteredTodos = todos.filter(todo => todo.is_completed !== true);
+	} else if (state.filter === "completed") {
+		filteredTodos = todos.filter(todo => todo.is_completed === true);
+	}
+
+	const todoList = [];
+	filteredTodos.forEach((todo, index) => {
+		todoList.push(<TodoItem key={index} index={index} todo={todo} />);
+	});
+
+	return (
+		<Fragment>
+		<div className="todoListWrapper">
+		<ul>{todoList}</ul>
+		</div>
+
+		<TodoFilters
+		todos={filteredTodos}
+		currentFilter={state.filter}
+		filterResultsFn={filterResults}
+		clearCompletedFn={clearCompleted}
+		clearInProgress={state.clearInProgress}
+		/>
+		</Fragment>
+	);
 };
 
 const TodoPrivateListQuery = () => {
